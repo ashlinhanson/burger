47269db4615bf91b43d21d7e49e8fb6ce000ad5f
ashlinhanson <ashlinhanson@gmail.com>
2020-10-16 13:36:51 -0500

    server.js file completed

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// set handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// import routes and give the server access to them
+const routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+//start the server to begin listening to the client requests
+app.listen(PORT, function(){
+    console.log("Server is listening on: http://localhost:" + PORT);
+});
