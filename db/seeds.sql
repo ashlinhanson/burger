9569683523c47d7c2601707bec8618e8ef8c8169
ashlinhanson <ashlinhanson@gmail.com>
2020-10-16 13:25:26 -0500

    seeds completed

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Plain Jane");
+INSERT INTO burgers (burger_name) VALUES ("Double or Nothin'");
+INSERT INTO burgers (burger_name) VALUES ("Turkey Trot");
+INSERT INTO burgers (burger_name) VALUES ("Impossible ");
