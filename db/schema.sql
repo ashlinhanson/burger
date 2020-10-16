93dfe2019e309ea7114995ac32c3af7b4a0ed17f
ashlinhanson <ashlinhanson@gmail.com>
2020-10-16 13:19:41 -0500

    schema edited

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(50),
-    devoured BOOLEAN,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 );
