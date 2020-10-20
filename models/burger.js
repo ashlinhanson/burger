aaf1254c5d8688d9c024e34fee2935e3d7d3f3d6
ashlinhanson <ashlinhanson@gmail.com>
2020-10-20 13:02:13 -0500

    burger model

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require("../config/orm.js");
+
+let burger = {
+    selectAll : function(cb) {
+        orm.selectAll("burgers", function (result){
+            cb(result)
+        })
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(result){
+            cb(result);
+        })
+    },
+    updateOne: function(objColVals, condition, cb){
+        orm.updateOne("burgers", objColVals, condition, function(result){
+            cb(result);
+        })
+    }
+};
+
+module.exports = burger
