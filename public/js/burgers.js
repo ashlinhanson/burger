d2a0d530302c3bd49c9a3d0ef0c8f3cddf478fe4
ashlinhanson <ashlinhanson@gmail.com>
2020-10-20 12:12:38 -0500

    frontend js and jquery done

---
+++ public/js/burgers.js
@@ -0,0 +1,52 @@
+$(function(){
+    $(".change-devoured").on("click", function(event){
+        let id= $(this).data("id");
+        let newDevour = $(this).data("newdevoured");
+
+        let newDevourState = {
+            devoured: newDevour
+        };
+
+        $.ajax("/api/burgers" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function(){
+                console.log("changed devoured to" + newDevour);
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event){
+        event.preventDefault();
+
+        let newBurger = {
+            burger_name: $("#burger").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("added new burger");
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete-burger").on("click", function(event){
+        let id = $(this).data('id');
+
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function(){
+                console.log("deleted burger", id);
+                location.reload();
+            }
+        );
+    });
+});
